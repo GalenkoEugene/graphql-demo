cb5b499ded60264739c5b8a99012a12cb80cc6f7
Yevhenii Halenko <Re4port@ukr.net>
2024-05-15 10:26:38 +0300

    Update readme

--- app/graphql/mutations/create_comment.rb
+++ app/graphql/mutations/create_comment.rb
@@ -1,27 +1,31 @@
-class Mutations::CreateComment < Mutations::BaseMutation
-  null true
-  argument :body, String
-  argument :post_id, ID, loads: Types::PostType, as: :post# works only with global_id (take a look at GraphqlDemoSchema#object_from_id)
+# frozen_string_literal: true
 
-  # in next case resolver will be "resolve(body:, post_id:)". And manually we could find a post "Post.find(post_id)"
-  # argument :post_id, ID
+module Mutations
+  class CreateComment < Mutations::BaseMutation
+    null true
+    argument :body, String
+    argument :post_id, ID, loads: Types::PostType, as: :post # works only with global_id (take a look at GraphqlDemoSchema#object_from_id)
 
-  field :comment, Types::CommentType
-  field :errors, [String], null: false
+    # in next case resolver will be "resolve(body:, post_id:)". And manually we could find a post "Post.find(post_id)"
+    # argument :post_id, ID
 
-  # The #resolve method should return a hash whose symbols match the field names.
-  def resolve(body:, post:)
-    comment = post.comments.build(body: body)
-    if comment.save
-      {
-        comment: comment,
-        errors: [],
-      }
-    else
-      {
-        comment: nil,
-        errors: comment.errors.full_messages
-      }
+    field :comment, Types::CommentType
+    field :errors, [String], null: false
+
+    # The #resolve method should return a hash whose symbols match the field names.
+    def resolve(body:, post:)
+      comment = post.comments.build(body:)
+      if comment.save
+        {
+          comment:,
+          errors: []
+        }
+      else
+        {
+          comment: nil,
+          errors: comment.errors.full_messages
+        }
+      end
     end
   end
 end
